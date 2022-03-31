694d75b39ceb40b31859fc22671fd36689800363
Yerong Li <yerongli.ads@outlook.com>
2022-03-31 00:43:20 -0500

    move files

--- data/vg8k/move.py
+++ data/vg8k/move.py
@@ -1,1 +1,10 @@
 import os
+  
+source = './VG_100K/images/'
+destination = './VG_100K/'
+  
+allfiles = os.listdir(source)
+  
+for f in allfiles:
+    print(f)
+    # os.rename(source + f, destination + f)
